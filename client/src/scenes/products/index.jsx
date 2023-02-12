e2b1dd89d7c1257465013d3e863b89e7fd2dd822
Srand <lishuoran@SranddeMacBook-Air.local>
2023-02-12 16:18:33 +0800

    0212

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Header from "components/Header";
+import { useGetProductsQuery } from "state/api";
 
 const Products = () => {
+  const { data, isLoading } = useGetProductsQuery();
+  const isNonMobile = useMediaQuery("(min-width:1000px)");
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box m="1.5rem 2.5rem">
+      <Header title="Products" subtitle="See your list of products." />
+      {data || !isLoading ? (
+        <Box
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4,minmax(0,1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            "& > div": { gridColumn: isNonMobile? undefined : "span 4"}
+          }}
+        >
+          {data.map()}
+        </Box>
+      ) : (
+        <>Loading...</>
+      )}
+    </Box>
+  );
+};
 
-export default Products
+export default Products;
